e2cd334b3fe0737a7b135bc9d83e90c9ad2a7a20
Mchoffn <mchoffn@hotmail.com>
2020-05-02 15:18:45 +0200

    opdatering af zoom

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -33,8 +33,8 @@ export default {
         renderMap(){
             const map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: this.lat, lng: this.lng },
-                zoom: 6,
-                maxZoom: 15,
+                zoom: 20,
+                maxZoom: 20,
                 minZoom: 3,
                 streetViewControl: false
             })
